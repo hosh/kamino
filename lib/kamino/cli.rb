b7f572565dd6d44e00869c417b6a65da404fea8d
Ho-Sheng Hsiao <hosh@opscode.com>
2013-03-27 21:26:38 -0700

    [CMD] Added `kamino rails`

--- lib/kamino/cli.rb
+++ lib/kamino/cli.rb
@@ -5,7 +5,31 @@ module Kamino
     include Thor::Actions
 
     def self.source_paths
-      File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'templates'))
+      [ File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'templates')) ]
+    end
+
+    desc 'rails', "Makes your Rails app cloneable"
+    def rails
+      target = Dir.pwd
+      template File.join('rails', 'Berksfile.erb'), File.join(target, 'Berksfile')
+      template File.join('rails', 'Vagrantfile.erb'), File.join(target, 'Vagrantfile')
+      append_file(File.join(target, 'Gemfile')) { <<END }
+
+# Berkshelf manages your cookbooks like bundler
+# Define your OS dependencies in Berksfile
+group :kamino do
+  gem 'berkshelf'
+end
+END
+
+      append_file(File.join(target, '.gitignore')) { <<END }
+.vagrant
+.vagrant.*
+END
+
+      empty_directory File.join(target, 'vendor', 'cookbooks')
+      create_file     File.join(target, 'vendor', 'cookbooks', '.keep'), ''
+
     end
   end
 end
